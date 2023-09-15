a8c3ac4850a1853318cc260fcbf7599e56987f4a
crzytrane <crzytrane@gmail.com>
2023-09-15 23:24:11 +1200

    nvChad config

---
+++ configs/overrides.lua
@@ -0,0 +1,70 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    -- "c-sharp", -- not sure what this should be 
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+
+    -- c#
+    "omnisharp",
+    -- "csharpier",
+    -- f#
+    -- "fsautocomplete",
+
+    -- docker
+    -- "docker-compose-language-service",
+    -- "dockerfile-language-server",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
